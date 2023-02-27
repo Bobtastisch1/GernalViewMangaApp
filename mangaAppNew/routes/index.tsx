1ee48ab6c2430935c5514b7940dadc13e372a19b
Erik Jerber <Erik.Jerber@edvring.de>
2023-02-27 14:08:08 +0100

    some stilying

--- mangaAppNew/routes/index.tsx
+++ mangaAppNew/routes/index.tsx
@@ -7,23 +7,15 @@ export default function Home() {
   return (
     <>
       <Head>
-        <title>Fresh App</title>
+        <title>Fresh App Home</title>
       </Head>
+     
       <div class="p-4 mx-auto max-w-screen-md">
-        <img
-          src="/logo.svg"
-          class="w-32 h-32"
-          alt="the fresh logo: a sliced lemon dripping with juice"
-        />
-        <p class="my-6">
-          Welcome to `fresh`. Try updating this message in the ./routes/index.tsx
-          file, and refresh.
-        </p>
+        <img src="/Cool.ico" class="object-none w-32 h-32 border-solid border-4 border-black"/>
         <Counter start={3} />
-        
         <button onClick="window.location.href='api/Numbers'" ><IconBrandAmongus class="w-9 h-9" ></IconBrandAmongus></button>
         <button onClick="window.location.href='api/Manga'" ><IconLego class="w-9 h-9" /></button>
-        <button onClick="window.location.href='api/Anime'" ><IconMoodSing class="w-9 h-9" ></IconMoodSing></button>
+        <button onClick="window.location.href='api/Anime'" ><IconMoodSing class="w-9 h-9" ></IconMoodSing></button>    
       </div>
     </>
   );
